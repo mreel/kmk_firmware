d5473a2c0ac6503c085f6dd9700ff8c2e8f04967
mreel <mistermister1111@hotmail.com>
2023-11-14 21:10:54 +0100

    Added coord_mapping

--- boards/cobra_cadence/main.py
+++ boards/cobra_cadence/main.py
@@ -9,10 +9,13 @@ split = Split(split_flip=False, split_type=SplitType.UART, data_pin=keyboard.dat
 keyboard.modules.append(split)
 
 keyboard.keymap = [
-    [KC.ESC, KC.W, KC.D, KC.F, KC.G, KC.Y, KC.U, KC.R, KC.L, KC.N0,
-     KC.Q, KC.S, KC.E, KC.T, KC.B, KC.P, KC.N, KC.I, KC.O, KC.J,
-     KC.A, KC.X, KC.C, KC.V, KC.NO, KC.NO, KC.M, KC.K, KC.COMM, KC.H,
-     KC.Z, KC.NO, KC.N1, KC.SPC, KC.N2, KC.N3, KC.ENT, KC.BSPC, KC.NO, KC.DOT]
+    [   # Base
+        KC.ESC,                                                                KC.N0,
+        KC.Q, KC.W, KC.D,  KC.F,   KC.G,      KC.Y,   KC.U,    KC.R,  KC.L,    KC.J,
+        KC.A, KC.S, KC.E,  KC.T,                      KC.N,    KC.I,  KC.O,    KC.H,
+        KC.Z, KC.X, KC.C,  KC.V,   KC.B,      KC.P,   KC.M,    KC.K,  KC.COMM, KC.DOT,
+                    KC.N1, KC.SPC, KC.TAB,    KC.ENT, KC.BSPC, KC.DEL
+    ]
 ]
 
 if __name__ == '__main__':
